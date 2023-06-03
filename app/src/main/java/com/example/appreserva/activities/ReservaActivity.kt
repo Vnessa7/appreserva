dd0d9d5a84775c5e711e6e018552e9389fd05542
Vnessa7 <vanessa_garciad@hotmail.com>
2023-06-02 23:15:19 -0500

    home listo

--- app/src/main/java/com/example/appreserva/activities/ReservaActivity.kt
+++ app/src/main/java/com/example/appreserva/activities/ReservaActivity.kt
@@ -1,4 +1,13 @@
 package com.example.appreserva.activities
 
-class ReservaActivity {
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appreserva.R
+import android.os.Bundle
+
+class ReservaActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserva)
+    }
 }
